6634d0fcc3a1b6a15a824c9b632987d099f0c37e
Ian Chin <skyhawk3102000@yahoo.com>
2021-12-09 17:36:01 -0500

    additional content

--- base/views.py
+++ base/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # Create your views here.
 
@@ -16,12 +17,12 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect("home") 
 
-
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -34,12 +35,28 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username OR password does not exist')
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # the commit=False is freezing the save so we can process the data before save
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            # TODO: Change here to make sure the user doesnt login immediately and has to verify email
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form':form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -58,7 +75,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    # how to take model data and use it in other models
+    roomMessages = room.message_set.all()
+    if request.method == 'POST':
+        msg= Message.objects.create(
+            user= request.user,
+            room= room,
+            body= request.POST.get('room_body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'roomMessages':roomMessages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
